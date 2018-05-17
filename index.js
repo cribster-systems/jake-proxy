27c1f04b07a18d4a19b3b1a2b030423d193ae1d5
Jake Kopulsky <jakekopulsky@gmail.com>
2018-05-17 14:13:17 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+app.use('/:location_id', express.static('index.html'));
+
+app.listen(8080, () => console.log('Proxy server listening on port 8080!'));
