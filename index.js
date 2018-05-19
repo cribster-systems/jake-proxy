634d8996f43bea51f2c5988480789005997d71bf
Jake Kopulsky <jakekopulsky@gmail.com>
2018-05-18 20:06:33 -0700

    dockerize proxy

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 require('newrelic');
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
 
 const app = express();
 app.use(cors());
 
-app.use('/:location_id', express.static('index.html'));
+// if (cluster.isMaster) {
+//   console.log(`Master ${process.pid} is running`);
 
-app.listen(8080, () => console.log('Proxy server listening on port 8080!'));
+//   // Fork workers.
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//   });
+  
+// } else {
+
+  app.use('/:location_id', express.static('index.html'));
+  app.get('/', (req, res) => {
+    res.sendStatus(200);
+  });
+
+  app.listen(8080, () => console.log('Proxy server listening on port 8080!'));
+// }
